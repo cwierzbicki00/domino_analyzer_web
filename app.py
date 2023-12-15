485623fd856cbd222afe6817efb0c135432851d4
Ryan S <114497815+rsmith531@users.noreply.github.com>
2023-12-15 17:24:46 -0500

    Update app.py
    Add conditional logic to make Flask app work on PythonAnywhere

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def model_files(filename):
     return send_from_directory('yolov7_custom', filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    if 'liveconsole' not in gethostname():
+        app.run(host='0.0.0.0', debug=True)
